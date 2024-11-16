2806b8425bd3455c7e8a5e6ecdc2e1c5fa47a3eb
Axel Cocat <ax.cocat@gmail.com>
2024-11-16 23:28:14 +0100

    feat(ui): convert images to webp

--- ui/src/images.ts
+++ ui/src/images.ts
@@ -1,9 +1,9 @@
-import bastionImg from "./assets/img/characters/Bastion_Portrait_mini.png";
-import echoImg from "./assets/img/characters/Echo_Portrait_mini.png";
-import hybridImg from "./assets/img/characters/Hybrid_Portrait_mini.png";
-import mediatorImg from "./assets/img/characters/Mediator_Portrait_mini.png";
-import nidumImg from "./assets/img/characters/Nidum_Portrait_mini.png";
-import pandoraImg from "./assets/img/characters/Pandora_Portrait_mini.png";
-import techHunterImg from "./assets/img/characters/TechHunter_Portrait_mini.png";
+import bastionImg from "./assets/img/characters/Bastion_Portrait_mini.webp";
+import echoImg from "./assets/img/characters/Echo_Portrait_mini.webp";
+import hybridImg from "./assets/img/characters/Hybrid_Portrait_mini.webp";
+import mediatorImg from "./assets/img/characters/Mediator_Portrait_mini.webp";
+import nidumImg from "./assets/img/characters/Nidum_Portrait_mini.webp";
+import pandoraImg from "./assets/img/characters/Pandora_Portrait_mini.webp";
+import techHunterImg from "./assets/img/characters/TechHunter_Portrait_mini.webp";
 
 export const characterImages = [hybridImg, nidumImg, pandoraImg, bastionImg, techHunterImg, mediatorImg, echoImg];
