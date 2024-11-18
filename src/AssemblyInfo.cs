6c885def43e6347ae4e7c4395274932fa050b6e9
Axel Cocat <ax.cocat@gmail.com>
2024-11-18 23:52:24 +0100

    v1.0.1

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using Gatemeter;
 using MelonLoader;
 
-[assembly: MelonInfo(typeof(GatemeterMod), "Gatemeter", "1.0.0", "github.com/r-o-b-o-t-o")]
+[assembly: MelonInfo(typeof(GatemeterMod), "Gatemeter", "1.0.1", "github.com/r-o-b-o-t-o")]
 [assembly: MelonGame("Gravity Lagoon", "Gatekeeper")]
